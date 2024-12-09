9f8c9cfbc3cb2752aacb7871bb870a01d620ace5
vrit-prashant <prashant@vrittechnologies.com>
2024-12-09 14:58:40 +0545

    logo image return url in college

--- collegemanagement/viewsets/college_viewsets.py
+++ collegemanagement/viewsets/college_viewsets.py
@@ -4,7 +4,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from ..models import College
 from ..serializers.college_serializers import CollegeListSerializers, CollegeRetrieveSerializers, CollegeWriteSerializers
 from ..utilities.importbase import *
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 class collegeViewsets(viewsets.ModelViewSet):
     serializer_class = CollegeListSerializers
     # permission_classes = [collegemanagementPermission]
@@ -41,4 +42,21 @@ class collegeViewsets(viewsets.ModelViewSet):
     # @action(detail=False, methods=['get'], name="action_name", url_path="url_path")
     # def action_name(self, request, *args, **kwargs):
     #     return super().list(request, *args, **kwargs)
+    
+    @action(detail=True, methods=['get'], url_path="college-logo")
+    def get_dp_image(self, request, pk=None):
+        try:
+            # Fetch the college by primary key
+            college = self.get_object()
+        except College.DoesNotExist:
+            return Response({"error": "College not found."}, status=404)
+
+        # Check if dp_image exists
+        if not college.dp_image:
+            return Response({"message": "No display image available for this college."}, status=404)
+
+        # Construct the absolute URL for the dp_image
+        dp_image_url = request.build_absolute_uri(college.dp_image.url)
+
+        return Response({"college_name": college.name, "dp_image_url": dp_image_url}, status=200)
 
