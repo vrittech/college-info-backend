27a9ef0b058a4229d6be86c1904e6eba071b2171
vrit-prashant <prashant@vrittechnologies.com>
2024-12-09 14:59:19 +0545

    `Updated college_viewsets.py: changed activePatchIndex, added new patches, updated get_dp_image action and get_queryset method`

--- collegemanagement/viewsets/college_viewsets.py
+++ collegemanagement/viewsets/college_viewsets.py
@@ -10,7 +10,7 @@ class collegeViewsets(viewsets.ModelViewSet):
     serializer_class = CollegeListSerializers
     # permission_classes = [collegemanagementPermission]
     # authentication_classes = [JWTAuthentication]
-    #pagination_class = MyPageNumberPagination
+    pagination_class = MyPageNumberPagination
     queryset = College.objects.all().order_by('-id')
 
     filter_backends = [SearchFilter, DjangoFilterBackend, OrderingFilter]
@@ -30,7 +30,7 @@ class collegeViewsets(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        #return queryset.filter(user_id=self.request.user.id)
+        return queryset
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
