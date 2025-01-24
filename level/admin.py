e0841fb3a87b778055a993af3680e11f5de2c11b
vrit-prashant <prashant@vrittechnologies.com>
2025-01-24 11:50:40 +0545

    `Update admin.py to register Level and SubLevel models`

--- level/admin.py
+++ level/admin.py
@@ -2,5 +2,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Level
+from .models import Level,SubLevel
 admin.site.register(Level)
+admin.site.register(SubLevel)
