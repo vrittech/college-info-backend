06823819429cb2964c346a128688ef0079675ebd
vrit-prashant <prashant@vrittechnologies.com>
2025-01-28 11:58:15 +0545

    `Update models.py: fix activePatchIndex, add new patches, and update Level model`

--- level/models.py
+++ level/models.py
@@ -19,7 +19,7 @@ class SubLevel(models.Model):
         ]
     
 class Level(models.Model):
-    sublevel = models.ManyToManyField(SubLevel,related_name='sublevel_level')
+    sublevel = models.ManyToManyField(SubLevel,related_name='sublevel_level',blank=True)
     name = models.CharField(max_length=255,null=True,blank=True)
     description = models.TextField(null=True,blank=True)
     image = models.ImageField(upload_to='level/',null=True,blank=True)
