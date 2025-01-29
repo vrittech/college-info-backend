607982bd968cbc8216014a18c939fb99bad4c273
vrit-prashant <prashant@vrittechnologies.com>
2025-01-29 14:45:49 +0545

    information

--- informationmanagement/serializers/information_serializers.py
+++ informationmanagement/serializers/information_serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.db import transaction
+
 from ..models import Information, Level, SubLevel, Course, Affiliation, District, College, Faculty, InformationTagging, InformationCategory, InformationGallery
 
 class InformationTaggingSerializer(serializers.ModelSerializer):
@@ -31,110 +33,114 @@ class InformationRetrieveSerializers(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class InformationWriteSerializers(serializers.ModelSerializer):
-    level = serializers.PrimaryKeyRelatedField(queryset=Level.objects.all(), many=True, required=False)
-    sublevel = serializers.PrimaryKeyRelatedField(queryset=SubLevel.objects.all(), many=True, required=False)
-    course = serializers.PrimaryKeyRelatedField(queryset=Course.objects.all(), many=True, required=False)
-    affiliation = serializers.PrimaryKeyRelatedField(queryset=Affiliation.objects.all(), many=True, required=False)
-    district = serializers.PrimaryKeyRelatedField(queryset=District.objects.all(), many=True, required=False)
-    college = serializers.PrimaryKeyRelatedField(queryset=College.objects.all(), many=True, required=False)
-    faculty = serializers.PrimaryKeyRelatedField(queryset=Faculty.objects.all(), many=True, required=False)
-    information_tagging = serializers.PrimaryKeyRelatedField(queryset=InformationTagging.objects.all(), many=True, required=False)
-    information_category = serializers.PrimaryKeyRelatedField(queryset=InformationCategory.objects.all(), many=True, required=False)
-    image = serializers.PrimaryKeyRelatedField(queryset=InformationGallery.objects.all(), many=True, required=False)
+class InformationWriteSerializer(serializers.ModelSerializer):
+    """
+    Serializer for handling Many-to-Many fields as comma-separated values
+    and handling binary image uploads in form-data.
+    """
+
+    # Expecting image uploads as binary files in form-data
+    image = serializers.ListField(
+        child=serializers.ImageField(max_length=None, allow_empty_file=False, use_url=False),
+        write_only=True,
+        required=False
+    )
 
     class Meta:
         model = Information
         fields = '__all__'
 
+    def parse_comma_separated_values(self, field_name):
+        """
+        Parses comma-separated values (e.g., "1,2,3") from form-data into a list of integers.
+        """
+        field_value = self.initial_data.get(field_name, "")
+        if field_value:
+            try:
+                return [int(pk.strip()) for pk in field_value.split(",") if pk.strip()]
+            except ValueError:
+                raise serializers.ValidationError({field_name: "Must be a comma-separated list of integers."})
+        return []
+
+    @transaction.atomic
     def create(self, validated_data):
         """
-        Handles creation of Information instance with related objects based on provided ids.
+        Handles creation of Information instance with Many-to-Many fields and binary image uploads.
         """
-        # Extract the related fields (ManyToMany)
-        level_ids = validated_data.pop('level', [])
-        sublevel_ids = validated_data.pop('sublevel', [])
-        course_ids = validated_data.pop('course', [])
-        affiliation_ids = validated_data.pop('affiliation', [])
-        district_ids = validated_data.pop('district', [])
-        college_ids = validated_data.pop('college', [])
-        faculty_ids = validated_data.pop('faculty', [])
-        information_tagging_ids = validated_data.pop('information_tagging', [])
-        information_category_ids = validated_data.pop('information_category', [])
-        image_ids = validated_data.pop('image', [])
+        # Extract Many-to-Many relationships from form-data (comma-separated values)
+        level_ids = self.parse_comma_separated_values('level')
+        sublevel_ids = self.parse_comma_separated_values('sublevel')
+        course_ids = self.parse_comma_separated_values('course')
+        affiliation_ids = self.parse_comma_separated_values('affiliation')
+        district_ids = self.parse_comma_separated_values('district')
+        college_ids = self.parse_comma_separated_values('college')
+        faculty_ids = self.parse_comma_separated_values('faculty')
+        information_tagging_ids = self.parse_comma_separated_values('information_tagging')
+        information_category_ids = self.parse_comma_separated_values('information_category')
 
-        # Create the Information instance first
+        # Extract images from validated data
+        images = validated_data.pop('image', [])
+
+        # Create the Information instance
         information = Information.objects.create(**validated_data)
 
-        # Assign ManyToMany relationships
-        if level_ids:
-            information.level.set(level_ids)
-        if sublevel_ids:
-            information.sublevel.set(sublevel_ids)
-        if course_ids:
-            information.course.set(course_ids)
-        if affiliation_ids:
-            information.affiliation.set(affiliation_ids)
-        if district_ids:
-            information.district.set(district_ids)
-        if college_ids:
-            information.college.set(college_ids)
-        if faculty_ids:
-            information.faculty.set(faculty_ids)
-        if information_tagging_ids:
-            information.information_tagging.set(information_tagging_ids)
-        if information_category_ids:
-            information.information_category.set(information_category_ids)
-        if image_ids:
-            information.image.set(image_ids)
+        # Assign Many-to-Many relationships
+        information.level.set(level_ids)
+        information.sublevel.set(sublevel_ids)
+        information.course.set(course_ids)
+        information.affiliation.set(affiliation_ids)
+        information.district.set(district_ids)
+        information.college.set(college_ids)
+        information.faculty.set(faculty_ids)
+        information.information_tagging.set(information_tagging_ids)
+        information.information_category.set(information_category_ids)
+
+        # Save image uploads
+        for img in images:
+            InformationGallery.objects.create(information=information, image=img)
 
         return information
 
+    @transaction.atomic
     def update(self, instance, validated_data):
         """
-        Handles the update of Information instance with related objects based on provided ids.
+        Handles updating of Information instance with Many-to-Many fields and binary image uploads.
         """
-        # Extract the related fields (ManyToMany)
-        level_ids = validated_data.pop('level', [])
-        sublevel_ids = validated_data.pop('sublevel', [])
-        course_ids = validated_data.pop('course', [])
-        affiliation_ids = validated_data.pop('affiliation', [])
-        district_ids = validated_data.pop('district', [])
-        college_ids = validated_data.pop('college', [])
-        faculty_ids = validated_data.pop('faculty', [])
-        information_tagging_ids = validated_data.pop('information_tagging', [])
-        information_category_ids = validated_data.pop('information_category', [])
-        image_ids = validated_data.pop('image', [])
+        # Extract Many-to-Many relationships from form-data (comma-separated values)
+        level_ids = self.parse_comma_separated_values('level')
+        sublevel_ids = self.parse_comma_separated_values('sublevel')
+        course_ids = self.parse_comma_separated_values('course')
+        affiliation_ids = self.parse_comma_separated_values('affiliation')
+        district_ids = self.parse_comma_separated_values('district')
+        college_ids = self.parse_comma_separated_values('college')
+        faculty_ids = self.parse_comma_separated_values('faculty')
+        information_tagging_ids = self.parse_comma_separated_values('information_tagging')
+        information_category_ids = self.parse_comma_separated_values('information_category')
 
-        # Update the fields of the Information instance
+        # Extract images from validated data
+        images = validated_data.pop('image', [])
+
+        # Update instance fields
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
 
-        # Update ManyToMany relationships
-        if level_ids:
-            instance.level.set(level_ids)
-        if sublevel_ids:
-            instance.sublevel.set(sublevel_ids)
-        if course_ids:
-            instance.course.set(course_ids)
-        if affiliation_ids:
-            instance.affiliation.set(affiliation_ids)
-        if district_ids:
-            instance.district.set(district_ids)
-        if college_ids:
-            instance.college.set(college_ids)
-        if faculty_ids:
-            instance.faculty.set(faculty_ids)
-        if information_tagging_ids:
-            instance.information_tagging.set(information_tagging_ids)
-        if information_category_ids:
-            instance.information_category.set(information_category_ids)
-        if image_ids:
-            instance.image.set(image_ids)
+        # Update Many-to-Many relationships
+        instance.level.set(level_ids)
+        instance.sublevel.set(sublevel_ids)
+        instance.course.set(course_ids)
+        instance.affiliation.set(affiliation_ids)
+        instance.district.set(district_ids)
+        instance.college.set(college_ids)
+        instance.faculty.set(faculty_ids)
+        instance.information_tagging.set(information_tagging_ids)
+        instance.information_category.set(information_category_ids)
+
+        # Save new image uploads (append to existing images)
+        for img in images:
+            InformationGallery.objects.create(information=instance, image=img)
 
         return instance
-
 
 # {
 #   "title": "Sample Information",
