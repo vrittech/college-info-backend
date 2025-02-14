8b6920ad10ff76d9341b29a531f34bf189cb8a51
vrit-prashant <prashant@vrittechnologies.com>
2025-02-14 11:33:15 +0545

    college gallery

--- collegemanagement/serializers/collegegallery_serializers.py
+++ collegemanagement/serializers/collegegallery_serializers.py
@@ -12,6 +12,40 @@ class CollegeGalleryRetrieveSerializers(serializers.ModelSerializer):
         fields = '__all__'
 
 class CollegeGalleryWriteSerializers(serializers.ModelSerializer):
+    images = serializers.ListField(
+        child=serializers.ImageField(), write_only=True, required=False
+    )
+
     class Meta:
         model = CollegeGallery
-        fields = '__all__'
+        fields = ['images', 'description']  # Excluding 'image' field since we're handling multiple images
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        images = []
+        index = 0
+
+        while f'image[{index}]' in request.FILES:
+            images.append(request.FILES[f'image[{index}]'])
+            index += 1
+
+        gallery_instances = []
+        for image in images:
+            gallery_instance = CollegeGallery.objects.create(image=image)
+            gallery_instances.append(gallery_instance)
+
+        return gallery_instances
+
+    def update(self, instance, validated_data):
+        request = self.context.get('request')
+        images = []
+        index = 0
+
+        while f'image[{index}]' in request.FILES:
+            images.append(request.FILES[f'image[{index}]'])
+            index += 1
+
+        for image in images:
+            CollegeGallery.objects.create(image=image)
+
+        return super().update(instance, validated_data)
