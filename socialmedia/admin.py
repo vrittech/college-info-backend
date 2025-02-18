d75dd155e6b175a331b81cbad526c643122e6802
vrit-prashant <prashant@vrittechnologies.com>
2025-02-18 12:39:26 +0545

    "Added CollegeSocialMedia model registration to admin.py"

--- socialmedia/admin.py
+++ socialmedia/admin.py
@@ -2,5 +2,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import SocialMedia
+from .models import SocialMedia, CollegeSocialMedia
 admin.site.register(SocialMedia)
+admin.site.register(CollegeSocialMedia)
