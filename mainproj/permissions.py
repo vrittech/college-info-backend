6c18d64e296c7fff55b75b79f50c6fa139329505
Manoj Das <manoj@vrittechnologies.com>
2025-02-20 15:52:04 +0545

    permission fixed

--- mainproj/permissions.py
+++ mainproj/permissions.py
@@ -71,7 +71,6 @@ class DynamicModelPermission(BasePermission):
  
         group_permissions = get_group_permissions(request.user)
         required_permission = ACTION_PERMISSION_MAPPING.get(view.action, None)
-        print(group_permissions,"***********")
         # Enforce permission mapping (prevent unauthorized actions)
         if required_permission and group_permissions and required_permission in group_permissions.get(model_name.lower()):
             return True  # User's group does NOT have the required permission
@@ -90,7 +89,7 @@ class DynamicModelPermission(BasePermission):
             return True
             
         model_name = obj.__class__.__name__
-
+        print(model_name)
         # print(safe_model_method,"safe_model_method")
         public_object = safe_model_method.filter(model_name__model_name__iexact=model_name)
         if public_object.exists():
@@ -99,20 +98,26 @@ class DynamicModelPermission(BasePermission):
 
         group_permissions = get_group_permissions(request.user)
         required_permission = ACTION_PERMISSION_MAPPING.get(view.action, None)
-             
+        
+        # print(0)
         # Strictly check if the user's groups have permission for this model
-        if model_name not in group_permissions:
+        if model_name.lower() not in group_permissions:
             return False  # User's groups have NO permission for this model
 
+        # print(group_permissions)
+        
         # Enforce permission mapping at object level
-        if required_permission and group_permissions and required_permission in group_permissions.get(model_name):
+        if required_permission and group_permissions and group_permissions.get(model_name.lower()) and required_permission in group_permissions.get(model_name.lower()):
+            # print(1)
             # First layer ownership check
             for field in foreign_owner_field:
+                
                 if hasattr(obj, field):
+     
                     field_value = getattr(obj, field)
-                    
+
                     # Direct user ownership
-                    if field == 'user' and field_value == request.user:
+                    if field_value == request.user or request.user in field_value.all():
                         return True
                     
                     # Second layer ownership check (for fields like 'college')
@@ -128,7 +133,8 @@ class DynamicModelPermission(BasePermission):
                             # # Or if college has a ManyToMany relationship with users
                             # if hasattr(related_obj, 'users') and hasattr(related_obj.users, 'all'):
                             #     return request.user in related_obj.users.all()
-                
+            
+
             # If we reach here, the object doesn't have any of the expected ownership fields
             # or the user doesn't own the object
             return False
