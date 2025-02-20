737e56c1d4567a3001e1f3cf8ec1599fe1ebbf1c
vrit-prashant <prashant@vrittechnologies.com>
2025-02-20 10:52:55 +0545

    `Added new patches to eventgallery_viewsets.py`

--- event/serializers/eventgallery_serializers.py
+++ event/serializers/eventgallery_serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
-from ..models import EventGallery
+from ..models import EventGallery,Event
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['id', 'event_name','slug']
+
 
 class EventGalleryListSerializers(serializers.ModelSerializer):
+    event = EventSerializer(read_only=True)
     class Meta:
         model = EventGallery
         fields = '__all__'
 
 class EventGalleryRetrieveSerializers(serializers.ModelSerializer):
+    event = EventSerializer(read_only=True)
     class Meta:
         model = EventGallery
         fields = '__all__'
