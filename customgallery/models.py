7501f6891a2be561f288fafc6062b4dda7d2e52e
vrit-prashant <prashant@vrittechnologies.com>
2025-03-02 12:59:44 +0545

    `Fixed image field type in CustomGallery model.`

--- customgallery/models.py
+++ customgallery/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class CustomGallery(models.Model):
     name = models.CharField(max_length = 500 , null = True,blank = True)
-    image = models.ImageField(max_length = 500,upload_to = 'gallery/images')
+    image = models.FileField(max_length = 500,upload_to = 'gallery/images')
     created_date = models.DateField(auto_now_add=True, null = True,blank = True)
     created_date_time = models.DateTimeField(auto_now_add=True, null = True,blank = True)
     updated_date_time = models.DateTimeField(auto_now=True, null = True,blank = True)
