4de77e0829c3d714ca9416cbc97c2e28734790c6
vrit-prashant <prashant@vrittechnologies.com>
2025-03-05 14:56:25 +0545

    `Update eventgallery_viewsets.py: change activePatchIndex, add new patch, update queryset ordering`

--- event/viewsets/eventgallery_viewsets.py
+++ event/viewsets/eventgallery_viewsets.py
@@ -16,7 +16,7 @@ class eventgalleryViewsets(viewsets.ModelViewSet):
     permission_classes = [DynamicModelPermission]
     # authentication_classes = [JWTAuthentication]
     pagination_class = MyPageNumberPagination
-    queryset = EventGallery.objects.all().order_by('-id')
+    queryset = EventGallery.objects.all().order_by('-position')
 
     filter_backends = [SearchFilter, DjangoFilterBackend, OrderingFilter]
     search_fields = ['id']
