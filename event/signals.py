3bf63b9ccf5420bae3b73693e7d9a28ede7abc0a
vrit-prashant <prashant@vrittechnologies.com>
2025-03-05 12:49:22 +0545

    `Update signals.py to use EventGallery instead of Event and add a new patch.`

--- event/signals.py
+++ event/signals.py
@@ -1,8 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Event
+from .models import EventGallery
 
-@receiver(post_save, sender=Event)
+@receiver(post_save, sender=EventGallery)
 def set_position_same_as_id(sender, instance, created, **kwargs):
     if created and instance.position != instance.id:
         instance.position = instance.id
