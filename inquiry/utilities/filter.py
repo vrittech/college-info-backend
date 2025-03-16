590e2e3d47718e8701bffb9673d970d5853a616b
vrit-prashant <prashant@vrittechnologies.com>
2025-03-16 15:21:12 +0545

    `Added date filters to InquiryFilter and updated .env file with R2 credentials and settings`

--- inquiry/utilities/filter.py
+++ inquiry/utilities/filter.py
@@ -8,6 +8,15 @@ class InquiryFilter(filters.FilterSet):
      # Allow multiple course IDs
     courses_id = filters.BaseInFilter(field_name='courses__id', lookup_expr='in')  
     colleges_id = filters.BaseInFilter(field_name='colleges__id', lookup_expr='in')
+    
+    created_date = filters.DateFilter(field_name="created_date", lookup_expr="exact")
+    created_date__gte = filters.DateFilter(field_name="created_date", lookup_expr="gte")
+    created_date__lte = filters.DateFilter(field_name="created_date", lookup_expr="lte")
+    
+    updated_date = filters.DateFilter(field_name="updated_date", lookup_expr="exact")
+    updated_date__gte = filters.DateFilter(field_name="updated_date", lookup_expr="gte")
+    updated_date__lte = filters.DateFilter(field_name="updated_date", lookup_expr="lte")
+    
     class Meta:
         model = Inquiry
-        fields = ['courses', 'colleges','courses_id','colleges_id']
+        fields = ['courses', 'colleges','courses_id','colleges_id',"created_date","created_date__gte","created_date__lte","updated_date","updated_date__gte","updated_date__lte"]
