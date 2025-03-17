5429384f8ee88953dc961756f22d3ed41a53249d
vrit-prashant <prashant@vrittechnologies.com>
2025-03-17 11:12:36 +0545

    `Updated SocialMedia model in models.py to reflect changes in JSON schema.`

--- socialmedia/models.py
+++ socialmedia/models.py
@@ -7,7 +7,7 @@ class SocialMedia(models.Model):
     link = models.URLField(max_length=200)
     icon=models.ImageField(upload_to='components/banner',null = True,blank= True)
     is_show = models.BooleanField(default=False)
-    created_date = models.DateTimeField(auto_now_add=True, null = True,blank = True)
+    created_date = models.DateField(auto_now_add=True, null = True,blank = True)
     updated_date = models.DateTimeField(auto_now=True, null = True,blank = True)
     
     def __str__(self):
