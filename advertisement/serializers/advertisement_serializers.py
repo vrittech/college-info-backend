730ebf2db03a84bc6478a2d38eff63027ce722a8
vrit-prashant <prashant@vrittechnologies.com>
2025-03-18 10:26:28 +0545

    `Updated advertisement serializers with various changes to image and placement fields`

--- advertisement/serializers/advertisement_serializers.py
+++ advertisement/serializers/advertisement_serializers.py
@@ -23,16 +23,9 @@ class AdvertisementRetrieveSerializers(serializers.ModelSerializer):
 
 
 class AdvertisementWriteSerializers(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()  # Absolute URL for image
-    placement = PlacementPositionSerializers()  # Include nested object
+    image = serializers.SerializerMethodField(required=False)  # Absolute URL for image
+    placement = PlacementPositionSerializers(required=False)  # Include nested object
 
     class Meta:
         model = Advertisement
         fields = '__all__'  # Includes all fields + custom fields
-
-    def get_image(self, obj):
-        """Returns the absolute URL of the advertisement image"""
-        request = self.context.get('request')  # Get request object
-        if obj.image:
-            return request.build_absolute_uri(obj.image.url) if request else urljoin(settings.SITE_URL, obj.image.url)
-        return None  # If no image
