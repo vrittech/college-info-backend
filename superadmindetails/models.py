dede5f43e9d8811f159656b2cdb53c55459dc321
vrit-prashant <prashant@vrittechnologies.com>
2025-03-27 12:19:58 +0545

    `Updated SuperAdminDetails model with additional fields and updated permissions`

--- superadmindetails/models.py
+++ superadmindetails/models.py
@@ -4,11 +4,15 @@ from django.db import models
 from django.db import models
 
 class SuperAdminDetails(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=15)
-    location = models.CharField(max_length=255)
-    social_media_links = models.JSONField(blank=True, null=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    phone_number = models.CharField(max_length=15,null=True, blank=True)
+    location = models.CharField(max_length=255,null=True, blank=True)
+    messenger_link = models.CharField(max_length=510,null=True, blank=True)
+    facebook_link = models.CharField(max_length=510,null=True, blank=True)
+    x_link = models.CharField(max_length=510,null=True, blank=True)
+    youtube_link = models.CharField(max_length=510,null=True, blank=True)
+    linkedin_link = models.CharField(max_length=510,null=True, blank=True)
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
