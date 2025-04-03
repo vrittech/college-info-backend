38c4b85b4ee2f111897e5b563038354ccb7a088f
vrit-prashant <prashant@vrittechnologies.com>
2025-04-03 10:17:41 +0545

    permission in superadmin details

--- superadmindetails/utilities/permissions.py
+++ superadmindetails/utilities/permissions.py
@@ -1,43 +1,56 @@
+#------permission are classified into three types------->
+#first-level:-Admin,Superadmin,Superuser (this is  user model class which can be considered as ObjectA)
+#second-level:-object 'B' is assigned to user(i.e in object B , ObjectA is assigned), where user called as ObjectA
+#third-level:-object 'C' is assigned to object object B(i.e object B is assigned in object C)
+
+#model ObjectB->user field
+#model ObjectC->objectB field(objectB id)
+
+
+#as example, we can consider as , user,company,job where user is ObjectA,company is ObjectB,job is ObjectC
+
 from rest_framework.permissions import BasePermission
 
-SUPER_ADMIN_DETAILS_PERMISSIONS = {
-    "add": "add_superadmindetails",
-    "change": "change_superadmindetails",
-    "delete": "delete_superadmindetails",
-    "view": "view_superadmindetails",
-    "manage": "manage_super_admin_details",
-}
+SUPER_ADMIN = 1
+ADMIN = 2
 
-def HasPermission(request, codename):
-    """Check if the user has a specific Django permission"""
-    return request.user.has_perm(f"app_name.{codename}")
 
-def CanManage(request):
-    """Check if the user has full manage access"""
-    return HasPermission(request, SUPER_ADMIN_DETAILS_PERMISSIONS["manage"])
+def IsAuthenticated(request):
+    return bool(request.user and request.user.is_authenticated)
+
+def SuperAdminLevel(request):
+    return bool(IsAuthenticated(request) and request.user.is_superuser)
+
+def AdminLevel(request):
+    return bool(IsAuthenticated(request) and request.user.role in [ADMIN,SUPER_ADMIN])
+
+def isOwner(request):
+    if str(request.user.id) == str(request.data.get('user')):
+        return True
+    
+    elif len(request.data)==0 and len(request.POST)==0:
+        return True
+
+    return False
+
+
+# def ObjectBOwner(request):
+#     company = ObjectB.objects.filter(id = request.data.get('objectb'),user = request.user.id)
+#     if company.exists():
+#         return True
+#     return False
 
 class superadmindetailsPermission(BasePermission):
-    """
-    Permission class for managing super admin details based strictly on Django permissions.
-    """
-
     def has_permission(self, request, view):
-        if CanManage(request):  # If user has manage permission, grant full access
-            return True
-
         if view.action in ["list"]:
             return True
+        elif view.action in ['retrieve']:
+            return True
+        elif view.action in ['create','update']:
+            return AdminLevel(request) #second level
+            return ObjectBOwner(request) #third level
+        elif view.action == "partial_update":
+            return AdminLevel(request)
+        elif view.action == 'destroy':
+            return AdminLevel(request)
 
-        elif view.action in ["retrieve"]:
-            return HasPermission(request, SUPER_ADMIN_DETAILS_PERMISSIONS["view"])
-
-        elif view.action in ["create"]:
-            return HasPermission(request, SUPER_ADMIN_DETAILS_PERMISSIONS["add"])
-
-        elif view.action in ["update", "partial_update"]:
-            return HasPermission(request, SUPER_ADMIN_DETAILS_PERMISSIONS["change"])
-
-        elif view.action == "destroy":
-            return HasPermission(request, SUPER_ADMIN_DETAILS_PERMISSIONS["delete"])
-
-        return False  # Default deny access
