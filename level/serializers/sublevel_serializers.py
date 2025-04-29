f76ba1f2137178d2fbbbba6060fca773d31cd030
vrit-prashant <prashant@vrittechnologies.com>
2025-04-29 12:16:26 +0545

    `Added LevelSerializers and updated SubLevelListSerializers and SubLevelRetrieveSerializers to include level field with read_only=True`

--- level/serializers/sublevel_serializers.py
+++ level/serializers/sublevel_serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
-from ..models import SubLevel
+from ..models import SubLevel, Level
+
+class LevelSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        fields = ['id', 'name']
+        ref_name = 'LevelSerializers'
 
 class SubLevelListSerializers(serializers.ModelSerializer):
+    level = LevelSerializers(read_only=True)
     class Meta:
         model = SubLevel
         fields = '__all__'
 
 class SubLevelRetrieveSerializers(serializers.ModelSerializer):
+    level = LevelSerializers(read_only=True)
     class Meta:
         model = SubLevel
         fields = '__all__'
