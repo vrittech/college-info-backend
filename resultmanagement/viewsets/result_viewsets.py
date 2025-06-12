dcf63dca4ff5f6a2e604351ea150955fbd95446c
vrit-prashant <prashant@vrittechnologies.com>
2025-06-12 15:32:55 +0545

    update file_viewsets.py.json, result_viewsets.py.json, file_viewsets.py and result_viewsets.py

--- resultmanagement/viewsets/result_viewsets.py
+++ resultmanagement/viewsets/result_viewsets.py
@@ -26,7 +26,7 @@ from ..serializers.result_serializers import (
 from ..utilities.permissions import resultmanagementPermission
 from ..utilities.pagination import MyPageNumberPagination
 
-CACHE_TTL = 60 * 60 * 24 * 5   # cache for 5 days 
+CACHE_TTL = 60 * 60 * 24 * 20   # cache for 20 days 
 
 
 class resultViewsets(viewsets.ModelViewSet):
