7a4ad7aea3b438d7e80322e245d5b039cb862c13
Prawesh-SY <meetprawesh@hotmail.com>
2025-07-30 11:46:40 +0545

    admin url removed from college-info

--- mainproj/urls.py
+++ mainproj/urls.py
@@ -125,7 +125,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/',include('accountsmanagement.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
